b55a47ee73c0ee0392853f8ad145d1e867a4a5dd
Kyle Finch <kyle.finch.yg@renesas.com>
2024-08-28 20:44:33 +0000

    More formatting

--- boot/bootutil/include/bootutil/crypto/ecdsa.h
+++ boot/bootutil/include/bootutil/crypto/ecdsa.h
@@ -338,10 +338,10 @@ typedef struct {
  * \param[out]    size Pointer to a buffer containing the size of the public key extracted
  *
  */
-static inline void get_public_key_from_rfc5280_encoding(uint8_t **p, size_t  *size)
+static inline void get_public_key_from_rfc5280_encoding(uint8_t **p, size_t *size)
 {
-    uint8_t * key_start = (*p) + (PUB_KEY_LEN_OFF + 1 + (*p)[PUB_KEY_LEN_OFF] + PUB_KEY_VAL_OFF);
-    *p    = key_start;
+    uint8_t *key_start = (*p) + (PUB_KEY_LEN_OFF + 1 + (*p)[PUB_KEY_LEN_OFF] + PUB_KEY_VAL_OFF);
+    *p = key_start;
     *size = key_start[-2]-1; /* -2 from PUB_KEY_VAL_OFF to get the length, -1 to remove the ASN.1 padding byte count */
 }
 
@@ -536,15 +536,14 @@ static int bootutil_parse_eckey(bootutil_ecdsa_context *ctx, uint8_t **p, uint8_
       memcmp(alg.ASN1_CONTEXT_MEMBER(p), ec_pubkey_oid, sizeof(ec_pubkey_oid) - 1)) {
         return -3;
     }
-
 #if defined(MCUBOOT_SIGN_EC384)
     if (param.ASN1_CONTEXT_MEMBER(len) != sizeof(ec_secp384r1_oid) - 1 ||
       memcmp(param.ASN1_CONTEXT_MEMBER(p), ec_secp384r1_oid, sizeof(ec_secp384r1_oid) - 1)) {
         return -4;
     }
 #else
-    if (param.MBEDTLS_CONTEXT_MEMBER(len) != sizeof(ec_secp256r1_oid) - 1 ||
-      memcmp(param.MBEDTLS_CONTEXT_MEMBER(p), ec_secp256r1_oid, sizeof(ec_secp256r1_oid) - 1)) {
+    if (param.ASN1_CONTEXT_MEMBER(len) != sizeof(ec_secp256r1_oid) - 1 ||
+      memcmp(param.ASN1_CONTEXT_MEMBER(p), ec_secp256r1_oid, sizeof(ec_secp256r1_oid) - 1)) {
         return -4;
     }
 #endif
@@ -635,7 +634,7 @@ static inline int bootutil_ecdsa_verify(bootutil_ecdsa_context *ctx,
 
 #endif /* CY_MBEDTLS_HW_ACCELERATION */
 
-static inline int bootutil_ecdsa_parse_public_key(bootutil_ecdsa_context *ctx
+static inline int bootutil_ecdsa_parse_public_key(bootutil_ecdsa_context *ctx,
                                                   uint8_t **cp, uint8_t *end)
 {
     int rc;
